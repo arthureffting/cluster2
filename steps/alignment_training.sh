fff768c1afb78743d76f64c3d74612bdfbfa9449
arthur <effting@accuras.de>
2021-02-05 15:38:54 +0100

    Package update

--- steps/alignment_training.sh
+++ steps/alignment_training.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+#SBATCH --job-name=ALG_TR
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=2
+#SBATCH --gres=gpu:4
+#SBATCH --mem=12000
+#SBATCH -o /cluster/%u/cluster2/slurm/results/%j.out
+#SBATCH -e /cluster/%u/cluster2/slurm/results/%j.err
+#Timelimit format: "hours:minutes:seconds"
+#SBATCH --time=02:30:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=effting@accuras.de
 
-# Runs a python script wrapped in the arguments [same arguments for all scripts]
-/bin/bash steps/utils/bash_wrap.sh ${@} --script=aligned_training.py
+# Tell pipenv to install the virtualenvs in the cluster folder
+export WORKON_HOME==/cluster/$(whoami)/.python_cache
+export PYTHONPATH=.
+
+# Small Python packages can be installed in own home directory. Not recommended for big packages like tensorflow -> Follow instructions for pipenv below
+# cluster_requirements.txt is a text file listing the required pip packages (one package per line)
+
+pip3 install --user -r requirements.txt
+python3 scripts/original/steps/aligned_training.py --dataset=iam --model=original
